1c7f5acb030cf58180ea3e9ce615dbaeb407dcc7
unknown <alexanderlschalk@gmail.com>
2021-05-17 22:01:47 -0500

    Update package.json

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-var path = require('path')
-
-app.use(express.static(__dirname + '/'))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
-})
-app.listen(process.env.PORT || 4000)
